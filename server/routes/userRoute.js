0d5c872a492c803a4f6c71ce970eb2004bb22038
ridotoki <40660258+ridotoki@users.noreply.github.com>
2020-10-14 11:47:42 +0700

    Update login route without authen or authorize

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,26 @@
 const router = require('express').Router();
+const { compare } = require('bcrypt');
 const pool = require('../db');
 const checkAuth = require('../middleware/checkAuth');
 
 //login
 router.post('/login', async (req, res) => {
     try {
-        
+        const {email, password} = req.body;
+
+        const user = await pool.query("SELECT * FROM users WHERE email = $1", 
+        [email]);
+
+        if(user.rows.length === 0){
+            return res.status(401).json("Password or email is incorrect");
+        }
+
+        const validPassword = await compare(password, user.rows[0].password);
+
+        if(!validPassword){
+            return res.status(401).json("Password or email is incorrect");
+        }
+
     } catch (error) {
         
     }
