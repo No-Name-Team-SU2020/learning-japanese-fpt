c2500048bffb89a01aa97f1908031e364f26d00f
To Nam <namto2501@gmail.com>
2020-10-14 21:43:28 +0700

    view

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+var bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.use(express.json());
 app.use(cors());
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,29 +3,29 @@ const { compare } = require('bcrypt');
 const pool = require('../db');
 const bcrypt = require('bcrypt');
 const jwtGenerator = require('../utils/jwtGenerator');
-const checkAuth = require('../middleware/checkAuth');
+// const checkAuth = require('../middleware/checkAuth');
 
 //login
 router.post('/login', async (req, res) => {
     try {
-        const {email, password} = req.body;
+        const { email, password } = req.body;
 
-        const user = await pool.query("SELECT * FROM users WHERE email = $1", 
-        [email]);
+        const user = await pool.query("SELECT * FROM users WHERE email = $1",
+            [email]);
 
-        if(user.rows.length === 0){
-            return res.status(401).json("Password or email is incorrect");
+        if (user.rows.length === 0) {
+            return res.status(401).json("Password or email is incorrect1");
         }
 
         const validPassword = await bcrypt.compare(password, user.rows[0].password);
 
-        if(!validPassword){
-            return res.status(401).json("Password or email is incorrect");
+        if (!validPassword) {
+            return res.status(401).json("Password or email is incorrect2");
         }
 
         const token = jwtGenerator(user.rows[0].user_name);
 
-        return res.json({token}); //check if token is given
+        return res.json({ token }); //check if token is givens
 
     } catch (error) {
         console.error(error.message);
@@ -34,12 +34,27 @@ router.post('/login', async (req, res) => {
 });
 
 //logout
-router.post('/logout', async(req, res) => {
+router.post('/logout', async (req, res) => {
     try {
-        
+
     } catch (error) {
-        
+
     }
 });
-
 module.exports = router;
+
+//view user 
+router.post('/view', async (req, res) => {
+    try {
+        const { email } = req.body;
+        const user = await pool.query("Select display_name ,user_name, email from users where email = $1", [email]);
+        if (user.rows.length > 0) {
+            res.json(user.rows)
+        }else{
+            res.status(500).send("Can not found!");
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server error");
+    }
+});
