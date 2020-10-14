56de8936631612d5361fcc571c691a125c2eb371
ridotoki <40660258+ridotoki@users.noreply.github.com>
2020-10-14 11:37:33 +0700

    Adding userRoute file

---
+++ server/routes/userRoute.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const pool = require('../db');
+const checkAuth = require('../middleware/checkAuth');
+
+//login
+router.post('/login', async (req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+//logout
+router.post('/logout', async(req, res) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
