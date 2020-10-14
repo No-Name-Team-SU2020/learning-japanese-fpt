dfd163a005f6b134d73c7d8e0839bb70aad81a4d
ridotoki <40660258+ridotoki@users.noreply.github.com>
2020-10-14 14:04:19 +0700

    Update index file

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
+app.use(require('./routes/userRoute.js'));
+
 app.listen(3000, () => {
     console.log('Listening to port 3000');
 });
