440d916cedbad58b8682a4c0c4b6093cd6c9d2d1
ridotoki <40660258+ridotoki@users.noreply.github.com>
2020-10-15 23:13:37 +0700

    Update login and logout Add view profile by user_name

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 const pool = require('../db');
 const bcrypt = require('bcrypt');
+//const jwt = require('jsonwebtoken');
 const jwtGenerator = require('../utils/jwtGenerator');
 const jwtRefresher = require('../utils/jwtRefresher');
 const checkAuth = require('../middleware/checkAuth');
+const validInfo = require('../middleware/validInfo');
+
+let refreshTokens = [];
 
 //login
-router.post('/login', async (req, res) => {
+router.post('/login', validInfo, async (req, res) => {
     try {
         const {email, password} = req.body;
 
@@ -23,8 +27,9 @@ router.post('/login', async (req, res) => {
             return res.status(401).json("Password or email is incorrect");
         }
 
-        const token = jwtGenerator(user.rows[0].user_name);
-        const refreshToken = jwtRefresher(user.rows[0].user_name);
+        token = jwtGenerator(user.rows[0].user_name);
+        refreshToken = jwtRefresher(user.rows[0].user_name);
+        refreshTokens.push(refreshToken)
 
         return res.json({token, refreshToken}); //check if token is given
 
@@ -35,12 +40,31 @@ router.post('/login', async (req, res) => {
 });
 
 //logout
-router.delete('/logout', async(req, res) => {
+router.delete('/logout', checkAuth, async(req, res) => {
     try {
-        let refreshTokens = [];
         //const refreshToken = req.body.token;
         refreshTokens = refreshTokens.filter(token => token !== req.body.token)
-        res.sendStatus(204);
+        res.status(204).send("Logged out");
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server error");
+    }
+});
+
+router.get('/viewProfile', async(req, res) => {
+    try {
+        const {user_name} = req.body;
+        
+        const user = await pool.query("SELECT display_name, email FROM users WHERE user_name = $1",
+        [user_name]);
+
+        if(user.rows.length === 0){
+            res.status(404).send("User not found");
+        }
+        else{
+            res.json(user.rows[0]);
+        }
+
     } catch (error) {
         console.error(error.message);
         res.status(500).send("Server error");
