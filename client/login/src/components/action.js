bd50147633d1b23130d66cddd20546f78e1a0087
yusaki <yusakithejoker@gmail.com>
2020-10-22 02:11:49 +0700

    Update login file

---
+++ client/login/src/components/action.js
@@ -0,0 +1,2 @@
+//khởi tạo file userLoginFetch trong đó có chứa link API để kết nối với database\
+// tạo token = phiên đăng nhập
--- client/login/src/components/login.component.js
+++ client/login/src/components/login.component.js
@@ -2,57 +2,62 @@ import React, { Component } from "react";
 
 import Japan from '../../src/img/Japan.jpg';
 export default class Login extends Component {
+    state = {
+        loading: true,
+        person: null,
+        username: "",
+        password: ""
+    };
+    async componentDidMount() {
+        const url = "";
+        const responese = await fetch(url);
+        const data = await responese.json();
+        this.setState({ person: data.results[0] });
+        console.log(data);
+    }
     render() {
-        return (
-            <form>
-                <h3>Đăng nhập</h3>
-                <div className="form-group">
-                    <label>Email </label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
-                {/* <div className="wrap-input100">
-                    <input
-                        className="input100"
-                        type="text"
-                        name="email"
-                        maxLength="320"
-                        required
-                    />
-                    {/* <span className="focus-input100"></span> */}
-                {/* <span className="label-input100">Email</span>
-                </div> */}
+        return ( <
+            div > {
+                this.state.loading || !this.state.loading ? ( <
+                    div > Loading... < /div>
+                ) : ( <
+                    form >
+                    <
+                    h3 > Đăng nhập < /h3> <
+                    div className = "form-group" >
+                    <
+                    label > Email < /label> <
+                    input type = "email"
+                    className = "form-control"
+                    placeholder = "Enter email"
+                    value = { this.state. }
+                    /> <
+                    /div>
 
-                 <div className="form-group">
-                    <label>Mật Khẩu</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div> 
-                {/* <div className="wrap-input100">
-                    <input
-                        className="input100"
-                        type="text"
-                        name="email"
-                        maxLength="320"
-                        required
-                    />
-                    <span className="focus-input100"></span>  */}
-                {/* <span className="label-input100">Mat Khau</span>
-                </div>  */}
-                {/* <div className="form-group">
-<div className="custom-control custom-checkbox">
-    <input type="checkbox" className="custom-control-input" id="customCheck1" />
-    <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-</div>
-</div> */}
-                <div className="container-login100-form-btn">
-                    <button type="submit" className="login100-form-btn">
-                        Đăng nhập
-            </button>
-                </div>
-                {/* <button type="submit" className="btn-signin">Đăng nhập</button> */}
-                <p className="forgot-password text-right">
-                    Quên <a href="#">mật khẩu?</a>
-                </p>
-            </form>
+
+                    <
+                    div className = "form-group" >
+                    <
+                    label > Mật Khẩu < /label> <
+                    input type = "password"
+                    className = "form-control"
+                    placeholder = "Enter password" / >
+                    <
+                    /div>  <
+                    div className = "container-login100-form-btn" >
+                    <
+                    button type = "submit"
+                    className = "login100-form-btn" >
+                    Đăng nhập <
+                    /button> <
+                    /div> { /* <button type="submit" className="btn-signin">Đăng nhập</button> */ } <
+                    p className = "forgot-password text-right" >
+                    Quên < a href = "#" > mật khẩu ? < /a> <
+                    /p> <
+                    /form>
+                )
+            } <
+            /div>
         );
     }
 }
