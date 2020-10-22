fcfd7ae915ea091ba5dfe0026d2654485278141e
yusaki <yusakithejoker@gmail.com>
2020-10-22 10:14:54 +0700

    Update login file

--- client/login/src/components/action.js
+++ client/login/src/components/action.js
@@ -1,2 +1,19 @@
-//khởi tạo file userLoginFetch trong đó có chứa link API để kết nối với database\
-// tạo token = phiên đăng nhập
+export const userLoginFetch = user => {
+    return dispatch => {
+        return fetch("http://localhost:3000/login", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    Accept: 'application/json',
+                },
+                body: JSON.stringify({ user })
+            })
+            .then(resp => resp.json())
+            .then(data => {
+                if (data.message) {} else {
+                    localStorage.setItem("token", data.jwt)
+                    dispatch(loginUser(data.user))
+                }
+            })
+    }
+}
--- client/login/src/components/login.component.js
+++ client/login/src/components/login.component.js
@@ -1,63 +1,73 @@
-import React, { Component } from "react";
-
+import React, { Component } from 'react';
+import { userLoginFetch } from '../src/components/action.js';
+import { connect } from 'react-redux'
 import Japan from '../../src/img/Japan.jpg';
 export default class Login extends Component {
     state = {
         loading: true,
-        person: null,
         username: "",
         password: ""
     };
-    async componentDidMount() {
-        const url = "";
-        const responese = await fetch(url);
-        const data = await responese.json();
-        this.setState({ person: data.results[0] });
-        console.log(data);
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
     }
+    handleSubmit = event => {
+        event.preventDefault()
+        this.props.userLoginFetch(this.state)
+    }
+
+    // async componentDidMount(){
+    //     const url = "";
+    //     const responese = await fetch(url);
+    //     const data = await responese.json();
+    //     this.setState({person: data.results[0] });
+    //     console.log(data);
+    // }
+
     render() {
         return ( <
-            div > {
-                this.state.loading || !this.state.loading ? ( <
-                    div > Loading... < /div>
-                ) : ( <
-                    form >
-                    <
-                    h3 > Đăng nhập < /h3> <
-                    div className = "form-group" >
-                    <
-                    label > Email < /label> <
-                    input type = "email"
-                    className = "form-control"
-                    placeholder = "Enter email"
-                    value = { this.state. }
-                    /> <
-                    /div>
-
-
-                    <
-                    div className = "form-group" >
-                    <
-                    label > Mật Khẩu < /label> <
-                    input type = "password"
-                    className = "form-control"
-                    placeholder = "Enter password" / >
-                    <
-                    /div>  <
-                    div className = "container-login100-form-btn" >
-                    <
-                    button type = "submit"
-                    className = "login100-form-btn" >
-                    Đăng nhập <
-                    /button> <
-                    /div> { /* <button type="submit" className="btn-signin">Đăng nhập</button> */ } <
-                    p className = "forgot-password text-right" >
-                    Quên < a href = "#" > mật khẩu ? < /a> <
-                    /p> <
-                    /form>
-                )
-            } <
+            div >
+            <
+            form onSubmit = { this.handleSubmit } >
+            <
+            h3 > Đăng nhập < /h3> <
+            div className = "form-group" >
+            <
+            label > Email < /label> <
+            input type = "email"
+            className = "form-control"
+            placeholder = "Enter email"
+            value = { this.state.username }
+            onChange = "this.handleChange" / >
+            <
+            /div> <
+            div className = "form-group" >
+            <
+            label > Mật Khẩu < /label> <
+            input type = "password"
+            className = "form-control"
+            placeholder = "Enter password"
+            value = { this.state.password }
+            onChange = "this.handleChange" / >
+            <
+            /div>  <
+            div className = "container-login100-form-btn" >
+            <
+            button type = "submit"
+            className = "login100-form-btn" >
+            Đăng nhập <
+            /button> <
+            /div> <
+            /form> <
             /div>
         );
     }
 }
+const mapDispatchToProps = dispatch => ({
+    userLoginFetch: userInfo => dispatch(userLoginFetch(userInfo))
+})
+
+export default connect(null, mapDispatchToProps)(Login);
