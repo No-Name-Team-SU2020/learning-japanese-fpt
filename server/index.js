6c6341ad34e5f9e42e0582c1bc5eebe32371bed6
yusaki <yusakithejoker@gmail.com>
2020-11-08 07:37:15 +0700

    Upload code 8/11

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 
 app.use(require('./routes/userRoute.js'));
 app.use('/admin', checkAdmin, require('./routes/adminRoute.js'));
-app.use('/teacher', checkTeacher, require('./routes/teacherRoute.js'));
+// app.use('/teacher', checkTeacher, require('./routes/teacherRoute.js'));
 
 app.listen(3000, () => {
     console.log('Listening to port 3000');
