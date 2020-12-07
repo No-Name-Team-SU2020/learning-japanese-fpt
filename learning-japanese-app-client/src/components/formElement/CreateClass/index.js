361855b0a46a8e3b74c7d6d1f5e57f355ae5c570
yusaki <yusakithejoker@gmail.com>
2020-12-07 17:34:43 +0700

    Fix bug 7/12

--- learning-japanese-app-client/src/components/formElement/CreateClass/index.js
+++ learning-japanese-app-client/src/components/formElement/CreateClass/index.js
@@ -14,7 +14,6 @@ const CreateClassForm = () => {
 
   const [newClass, setNewClass] = useState({
     class_name: "",
-    class_id: "",
   });
 
   const handleChange = (e) => {
@@ -28,7 +27,6 @@ const CreateClassForm = () => {
     dispatch(createClass(newClass));
     setNewClass({
       class_name: "",
-      class_id: "",
     });
   };
   return (
@@ -36,7 +34,7 @@ const CreateClassForm = () => {
       <h1 className='border-bottom pb-2 text-center'>FPT EDUCATION</h1>
       <h3 className='my-4'>Create class</h3>
       <form onSubmit={submitHandler}>
-        <Grid container spacing={3}>
+        {/* <Grid container spacing={3}>
           <Grid item md={4}>
             Class ID:
           </Grid>
@@ -51,7 +49,7 @@ const CreateClassForm = () => {
               onChange={handleChange}
             />
           </Grid>
-        </Grid>
+        </Grid> */}
         <Grid container spacing={3}>
           <Grid item md={4}>
             Class name:
