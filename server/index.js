ed7c66e104b902f86ec369db2029bb6d6085c372
ridotoki <40660258+ridotoki@users.noreply.github.com>
2020-12-29 14:36:32 +0700

    Testing again

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(require('./routes/userRoute.js'));
 app.use('/shared', require('./routes/sharedRoute.js'));
 app.use('/student', require('./routes/studentRoute.js'));
-app.use('/admin', checkAdmin, require('./routes/adminRoute.js'));
+app.use('/admin', require('./routes/adminRoute.js'));
 app.use('/teacher', checkTeacher, require('./routes/teacherRoute.js'));
 
 const PORT = process.env.PORT || 5000;
