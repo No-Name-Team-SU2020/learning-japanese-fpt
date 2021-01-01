a95ea0c1def83b6669e670d09bd179cd29713cda
ridotoki <40660258+ridotoki@users.noreply.github.com>
2021-01-01 12:50:41 +0700

    Test deploy

--- server/db.js
+++ server/db.js
@@ -1,5 +1,6 @@
 //const Pool = require('pg').Pool
 const sequelize = require('sequelize');
+require('dotenv').config();
 
 // const pool = new Pool({
 //     user: "postgres",
@@ -10,20 +11,29 @@ const sequelize = require('sequelize');
 // });
 
 //New connecting method with sequelize
-const db = new sequelize('Capstone_Project', 'postgres', '09001210', {
-    host: 'localhost',
-    dialect: 'postgres',
+// const db = new sequelize('Capstone_Project', 'postgres', '09001210', {
+//     host: 'localhost',
+//     dialect: 'postgres',
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-      },
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//       },
 
-      define: {
-        timestamps: false
-      }
-  });
+//       define: {
+//         timestamps: false
+//       }
+//   });
+
+//deploy
+const db = new sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+
+    define: {
+      timestamps: false
+    }
+});
 
 module.exports = db;
